27a7b1380f8ec8858ea5ee380c06701a490de4cb
sonudeshwalgd <sd703299@gmail.com>
2026-01-04 04:42:37 +0530

    feat : push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
